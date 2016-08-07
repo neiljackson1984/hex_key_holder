aefbc387c8358873c14674cb74449d7d3b24057b
Neil Jackson <neiljackson1984@gmail.com>
2016-08-07 14:57:30 -0700

    add engraved labels.

--- equations.php
+++ equations.php
@@ -13,6 +13,13 @@ $defaultHexKey->widthAcrossFlats = 6 * $mm;
 $defaultHexKey->shortLegLength = 40 * $mm;
 $defaultHexKey->longLegLength = 100 * $mm;
 $defaultHexKey->bendRadius = 18 * $mm;
+$defaultHexKey->holsterHoleDiameter = 10 * $mm;
+$defaultHexKey->extraVerticalSlotDepth = 2 * $mm;
+$defaultHexKey->totalSlotDepth = 8 * $mm;
+$defaultHexKey->embedmentY = 20 * $mm;
+$defaultHexKey->embedmentZ = 50 * $mm;
+$defaultHexKey->holderExtentX = 25 * $mm;
+$defaultHexKey->funnelFilletRadius = 5 * $mm; //we are guaranteed to be safe if we keep this less than 1/2 * (totalSlotDepth - extraVerticalSlotDepth);
 
 // $hexKeys = 
 // [
@@ -57,8 +64,9 @@ $hexKey5->bendRadius     = $hexKey5->widthAcrossFlats * ($defaultHexKey->bendRad
 
 
 $hexKeyHolder->hexKeysIntervalX = 30 * $mm;
-$hexKeyHolder->hexKeysCount = 10;
+$hexKeyHolder->hexKeysCount = 7;
 $hexKeyHolder->hexKeysSpanX = $hexKeyHolder->hexKeysIntervalX * ($hexKeyHolder->hexKeysCount - 1);
 
+$hexKeyHolder->mountingSurfaceOffset = 20 * $mm;
 
 ?>
